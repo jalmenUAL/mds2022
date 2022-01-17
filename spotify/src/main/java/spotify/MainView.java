5e3875bc3c7eeffcdad64a6e746f6ed341154cd4
Administrator <Administrator@10.0.0.12>
2022-01-17 19:54:47 +0000

    Update

---
+++ spotify/src/main/java/spotify/MainView.java
@@ -0,0 +1,67 @@
+package spotify;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+import ventanas.VInicio;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and
+ * use @Route annotation to announce it in a URL as a Spring managed
+ * bean.
+ * Use the @PWA annotation make the application installable on phones,
+ * tablets and some desktop browsers.
+ * <p>
+ * A new instance of this class is created for every new user and every
+ * browser tab/window.
+ */
+@Route
+@PWA(name = "Vaadin Application",
+        shortName = "Vaadin App",
+        description = "This is an example Vaadin application.",
+        enableInstallPrompt = false)
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+
+    /**
+     * Construct a new Vaadin view.
+     * <p>
+     * Build the initial UI state for the user accessing the application.
+     *
+     * @param service The message service. Automatically injected Spring managed bean.
+     */
+    public MainView() {
+    	
+    	
+    	
+    	 VInicio v = new VInicio(); 
+         add(v);
+         v.getStyle().set("width", "100%");
+         v.getStyle().set("height", "100%");
+         v.getStyle().set("background", "black");
+         setSizeFull();
+         
+         
+     }
+ 
+       
+       
+        
+    
+
+}
---
+++ spotify/src/main/java/ventanas/VInicio.java
@@ -0,0 +1,60 @@
+package ventanas;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
+
+/**
+ * A Designer generated component for the v-inicio template.
+ *
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("v-inicio")
+@JsModule("./src/ventanas/v-inicio.ts")
+public class VInicio extends LitTemplate {
+
+    @Id("vaadinVerticalLayout")
+	private Element vaadinVerticalLayout;
+	@Id("vaadinButton")
+	private Button vaadinButton;
+	@Id("vaadinSplitLayout")
+	private SplitLayout vaadinSplitLayout;
+	@Id("vaadinVerticalLayout1")
+	private Element vaadinVerticalLayout1;
+	@Id("vaadinButton1")
+	private Button vaadinButton1;
+	@Id("vaadinButton2")
+	private Button vaadinButton2;
+	@Id("vaadinButton3")
+	private Button vaadinButton3;
+	@Id("vaadinVerticalLayout2")
+	private Element vaadinVerticalLayout2;
+	@Id("vaadinVerticalLayout3")
+	private Element vaadinVerticalLayout3;
+	@Id("vaadinVerticalLayout4")
+	private Element vaadinVerticalLayout4;
+	@Id("img")
+	private Image img;
+	@Id("img1")
+	private Image img1;
+	@Id("img2")
+	private Image img2;
+	@Id("label")
+	private Label label;
+	@Id("label1")
+	private Label label1;
+	/**
+     * Creates a new VInicio.
+     */
+    public VInicio() {
+        // You can initialise any data required for the connected UI components here.
+    }
+
+}
